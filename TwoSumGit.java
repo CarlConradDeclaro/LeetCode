870e5983e324d24faf1070e5aeff8bcd15a3cbe2
carlconrad <carlconrad98071234@gmail.com>
2023-09-01 20:52:00 +0800

    fist try

---
+++ TwoSumGit.java
@@ -0,0 +1,13 @@
+public class TwoSum {
+    public static void main(String args[]){
+
+        int arr[] = {3,2,3};
+        int target = 6;
+
+        SumOfTwo sumTwo = new SumOfTwo(arr,target);
+         sumTwo.getIndices();
+
+
+
+    }
+}
