38e850208148d52c95db0283df54099921f26074
carl conrad <carlconrad9801234@gmail.com>
2023-09-17 12:52:18 +0800

    change

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             for(int j=1;j<nums.length;j++){
              
-              
+             //change 
                 int   sum = nums[i] + nums[j];
               
 
